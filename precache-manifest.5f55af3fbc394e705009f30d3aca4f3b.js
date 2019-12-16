55647df22f3c03b615ab11f3b544779e1fd9e105
Kanon <kanonwang@outlook.com>
2019-12-16 14:35:06 +0800

    auto-deployed by gh-pages

---
+++ precache-manifest.5f55af3fbc394e705009f30d3aca4f3b.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8cfdcb47b2d78f3b80b",
+    "url": "main.js"
+  },
+  {
+    "revision": "44dd6b1b83d7413c08c89b09dd20a103",
+    "url": "index.html"
+  }
+];
